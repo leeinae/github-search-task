0849395e487be39662b30e4c6eef6edbf562ac72
inaeLee <inae9709@hanmail.net>
2021-10-23 23:48:14 +0900

    update: 프로젝트 폴더링 [#1]

---
+++ github-search-task/github-search-task/Sources/Network/Service/SearchService.swift
@@ -0,0 +1,8 @@
+//
+//  SearchService.swift
+//  github-search-task
+//
+//  Created by inae Lee on 2021/10/23.
+//
+
+import Foundation
---
+++ github-search-task/github-search-task/Sources/Scenes/SearchViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SearchViewController.swift
+//  github-search-task
+//
+//  Created by inae Lee on 2021/10/23.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    // MARK: - UIComponenets
+    
+    // MARK: - Properties
+    
+    // MARK: - Initializer
+    
+    // MARK: - LifeCycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setView()
+    }
+    
+    // MARK: - Actions
+    
+    // MARK: - Methods
+    
+    private func setView() {
+        view.backgroundColor = .white
+    }
+}
