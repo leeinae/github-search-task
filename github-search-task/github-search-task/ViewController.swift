1bbe0026790d3975e18cc3c4bdc0bfb85c52f483
inaeLee <inae9709@hanmail.net>
2021-10-23 22:10:40 +0900

    init: project 생성

---
+++ github-search-task/github-search-task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  github-search-task
+//
+//  Created by inae Lee on 2021/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
