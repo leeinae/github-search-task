5dffdf57f37a5c9beadc34322115bdeb97577e4f
leeinae <inae9709@hanmail.net>
2021-10-24 01:03:33 +0900

    [Feat/#1] 프로젝트 초기 셋팅 완료 (#5)
    * update: 프로젝트 폴더링 [#1]
    
    * add: SnapKit, Moya cocoapods 추가 [#1]

---
+++ github-search-task/Pods/Target Support Files/Pods-github-search-task-github-search-taskUITests/Pods-github-search-task-github-search-taskUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_github_search_task_github_search_taskUITests : NSObject
+@end
+@implementation PodsDummy_Pods_github_search_task_github_search_taskUITests
+@end
