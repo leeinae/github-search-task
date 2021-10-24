b85303c464b2eddcc75e79d4e8a143c6109c5706
inaeLee <inae9709@hanmail.net>
2021-10-24 20:00:38 +0900

    add: tableView, searchBar 추가 [#3]

--- github-search-task/github-search-task/Sources/Scenes/SearchViewController.swift
+++ github-search-task/github-search-task/Sources/Scenes/SearchViewController.swift
@@ -5,11 +5,24 @@
 //  Created by inae Lee on 2021/10/23.
 //
 
-import UIKit
 import SnapKit
+import UIKit
 
 class SearchViewController: UIViewController {
     // MARK: - UIComponenets
+
+    let searchBar: UISearchBar = {
+        let bar = UISearchBar()
+        bar.placeholder = "search repository"
+        
+        return bar
+    }()
+    
+    let tableView: UITableView = {
+        let table = UITableView()
+        
+        return table
+    }()
     
     // MARK: - Properties
     
@@ -21,6 +34,7 @@ class SearchViewController: UIViewController {
         super.viewDidLoad()
         
         setView()
+        setConstraints()
     }
     
     // MARK: - Actions
@@ -29,5 +43,16 @@ class SearchViewController: UIViewController {
     
     private func setView() {
         view.backgroundColor = .white
+        
+        navigationItem.titleView = searchBar
+        navigationController?.navigationItem.leftBarButtonItem = UIBarButtonItem(customView: searchBar)
+    }
+    
+    private func setConstraints() {
+        view.addSubview(tableView)
+        
+        tableView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
     }
 }
--- github-search-task/github-search-task/Supporting Files/SceneDelegate.swift
+++ github-search-task/github-search-task/Supporting Files/SceneDelegate.swift
@@ -14,7 +14,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
 
-        window?.rootViewController = SearchViewController()
+        window?.rootViewController = UINavigationController(rootViewController: SearchViewController())
         window?.makeKeyAndVisible()
     }
 
