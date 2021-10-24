1f6201ecc6c4cc45c9bde4c848cbf92b7f7eed70
inaeLee <inae9709@hanmail.net>
2021-10-24 21:30:12 +0900

    add: TableView  dequeueReusableCell, register extension 추가 [#3]

---
+++ github-search-task/github-search-task/Sources/Extensions/UITableView+.swift
@@ -0,0 +1,24 @@
+//
+//  UITableView+.swift
+//  github-search-task
+//
+//  Created by inae Lee on 2021/10/24.
+//
+
+import UIKit
+
+extension UITableView {
+    func dequeueReuableCell<T: UITableViewCell>(cell: T.Type, forIndexPath indexPath: IndexPath) -> T {
+        guard let cell = self.dequeueReusableCell(withIdentifier: String(describing: T.self), for: indexPath) as? T else {
+            fatalError("Unable Dequeue Reusable Cell")
+        }
+
+        return cell
+    }
+
+    func register<T: UITableViewCell>(cellType: T.Type) {
+        let identifier = String(describing: T.self)
+
+        self.register(cellType, forCellReuseIdentifier: identifier)
+    }
+}
