8fa852edfc595b9d7cc53c25e2d6ecf7938271c7
leeinae <inae9709@hanmail.net>
2021-10-24 21:33:33 +0900

    [Feat/#3] 전체 UI 구성 완료 (#6)
    * add: tableView, searchBar 추가 [#3]
    
    * add: TableView  dequeueReusableCell, register extension 추가 [#3]
    
    * add: SearchResultTableViewCell 추가 및 레이아웃 잡기 [#3]

--- github-search-task/github-search-task/Sources/Scenes/SearchViewController.swift
+++ github-search-task/github-search-task/Sources/Scenes/SearchViewController.swift
@@ -5,11 +5,27 @@
 //  Created by inae Lee on 2021/10/23.
 //
 
-import UIKit
 import SnapKit
+import UIKit
 
 class SearchViewController: UIViewController {
     // MARK: - UIComponenets
+
+    let searchBar: UISearchBar = {
+        let bar = UISearchBar()
+        bar.placeholder = "search repository"
+        
+        return bar
+    }()
+    
+    lazy var tableView: UITableView = {
+        let table = UITableView()
+        table.dataSource = self
+        table.delegate = self
+        table.register(cellType: SearchResultTableViewCell.self)
+        
+        return table
+    }()
     
     // MARK: - Properties
     
@@ -21,6 +37,7 @@ class SearchViewController: UIViewController {
         super.viewDidLoad()
         
         setView()
+        setConstraints()
     }
     
     // MARK: - Actions
@@ -29,5 +46,30 @@ class SearchViewController: UIViewController {
     
     private func setView() {
         view.backgroundColor = .white
+        
+        navigationItem.titleView = searchBar
+        navigationController?.navigationItem.leftBarButtonItem = UIBarButtonItem(customView: searchBar)
+    }
+    
+    private func setConstraints() {
+        view.addSubview(tableView)
+        
+        tableView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
     }
 }
+
+extension SearchViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        30
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: SearchResultTableViewCell = tableView.dequeueReuableCell(cell: SearchResultTableViewCell.self, forIndexPath: indexPath)
+        
+        return cell
+    }
+}
+
+extension SearchViewController: UITableViewDelegate {}
